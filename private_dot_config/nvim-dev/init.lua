9b33679d4c2de5ad610c99a6e9ab080c010c808e
kfujii-6jo <kfujii.dev@gmail.com>
2026-03-27 22:41:36 +0900

    feat: add nvim-dev config and fzf git branch aliases
    - Add nvim-dev minimal config for plugin development (NVIM_APPNAME)
    - Add gsw alias for fzf-based git branch switching
    - Add gbd alias for fzf-based git branch deletion (multi-select)

---
+++ private_dot_config/nvim-dev/init.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+opt.number = true
+opt.mouse = "a"
+opt.ignorecase = true
+opt.smartcase = true
+opt.wrap = false
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.cursorline = true
+opt.updatetime = 300
