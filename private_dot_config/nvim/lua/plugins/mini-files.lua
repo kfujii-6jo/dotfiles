2a426bacfe17696c7e4e87e6efe8e17cd3bccc87
kfujii-6jo <kfujii.dev@gmail.com>
2026-05-20 22:41:47 +0900

    fix(nvim): explicitly map <C-s> to synchronize in mini.files buffer

--- private_dot_config/nvim/lua/plugins/mini-files.lua
+++ private_dot_config/nvim/lua/plugins/mini-files.lua
@@ -24,6 +24,7 @@ return {
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
+        local buf = args.data.buf_id
         vim.keymap.set("n", "h", function()
           local state = mini_files.get_explorer_state()
           if state then
@@ -32,7 +33,10 @@ return {
               mini_files.go_out()
             end
           end
-        end, { buffer = args.data.buf_id, desc = "Go out (restricted to cwd)" })
+        end, { buffer = buf, desc = "Go out (restricted to cwd)" })
+        vim.keymap.set("n", "<C-s>", function()
+          mini_files.synchronize()
+        end, { buffer = buf, desc = "Synchronize (apply file operations)" })
       end,
     })
   end,
