f5c7c76b8d693cb41ebee0be003598e21803b81c
kfujii-6jo <kfujii.dev@gmail.com>
2026-06-26 23:00:40 +0900

    feat(nvim-dev): add lualine statusline

--- private_dot_config/nvim-dev/init.lua
+++ private_dot_config/nvim-dev/init.lua
@@ -46,6 +46,27 @@ require("lazy").setup({
     },
   },
   {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+  {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
